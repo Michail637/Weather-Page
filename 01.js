c852689166b8e420111f369ffdb14aa32eb47a25
Misha <kopitskyi.michail.49025@gmail.com>
2023-02-01 15:03:10 +0000

    Finished

--- 01.js
+++ 01.js
@@ -1,55 +1,91 @@
-// let btn = document.querySelector('.btn');
-
-// let city = prompt( "city input" )
-
-
-
-// let URLandKey = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=8TJJQRRLMR6LDYMDFBWG3Y5R8&contentType=json`;
-
-
-
-// btn.addEventListener( 'click', function(){
-//   fetch( URLandKey ).then( function(file){
-//     let curData = file.json();
-//     return curData
-//   } ).then( function(newObj) {
-//     console.log(newObj)
-//   } )
-// } )
-
-// function getDayName(dateStr, locale)
-// {
-//     var date = new Date(dateStr);
-//     return date.toLocaleDateString(locale, { weekday: 'long' });        
-// }
-
-// var dateStr = '05/23/2014';
-// var day = getDayName(dateStr, "nl-NL"); // Gives back 'Vrijdag' which is Dutch for Friday.
-
-// console.log(getDayName())
-
-
 let months = [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ];
+let days = [ 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday' ];
+let body = document.querySelector('body');
 
-let days = [ 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-
-
-let city = prompt( 'Which city would you like to know weather about ? ' );
+let city = prompt( 'Which city would you like to know the weather about ? ' );
 let URLandKey = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=8TJJQRRLMR6LDYMDFBWG3Y5R8&contentType=json`;
 
 fetch( URLandKey ).then( function(file){
   let curData = file.json();
   return curData;
 } ).then( function(newObj){
-  console.log(newObj)
-} )
+
+  function concatenate(){
+    let concatenation = ``;
+    for( let i = 1; i < 7; i++ ){
+      let htmlNew = `
+      <div class="element">
+      <div class="image">
+        <img src= images/${newObj.days[i].icon}.svg>
+      </div>
+      <div class="date-temp">
+        <div class="temp">${newObj.days[i].temp}</div>
+        <div class="day">${days[ new Date(newObj.days[i].datetime).getDay()]}</div>
+      </div>
+    </div>    
+      `
+      concatenation = concatenation + htmlNew;
+    }
+    return concatenation
+  }
 
 
+  let dateNumObj = newObj.days[0].datetime; // will always be the current date, as it is the 1st day from the object,  in format'01-02-2023'
+
+  let dayInNumbers = new Date(dateNumObj).getUTCDate(); // e.g. 15 or 31 or 25
+  let month = months[ new Date(dateNumObj).getMonth()]; // e.g. January, March etc.
+
+  let dayNumMonth = dayInNumbers + ' ' + month;  
+  let year = new Date( dateNumObj ).getFullYear();
+  
+
+  let htmlPage = `
+  <div class="container">
+  <header>
+    <div class="line"></div>
+    <div class="title">Weather</div>
+  </header>
 
 
+  <main>
+    <div class="image-block">
+      <div class="date-month">
+        <div class="number-date">${dayNumMonth}</div>
+        <div class="year">${year}</div>
+      </div>
+      <div class="weather-image">
+        <img src="images/${newObj.currentConditions.icon}.svg" alt="">
+        <div class="weather-condition">Cloudy</div>
+      </div>
+     
+    </div>
 
-// let datetime = '2023-02-01'
-// 
-// let day = datetime.getDay();
+    <div class="weather-right-hand-side">
+      <div class="up-block">
+        <p>Temperature</p>
+      </div>
+      <div class="middle-block">
+        <div class="temp-data">${newObj.currentConditions.temp}°</div>
+        <div class="temp-icon"><i class="fa-solid fa-temperature-three-quarters"></i></div>
+      </div>
+      <div class="down-block">
+        <p>Humidity <span class="humidity-data" >${newObj.currentConditions.humidity}%</span></p>
+        <div class="humidity-icon">
+          <img src="images/humidity.jpg" alt="">
+        </div>
+      </div>
+    </div>
+  </main>
 
-// console.log(day)
+
+  <footer>
+    <div class="slider">
+      ${concatenate()}
+    </div>
+  </footer>
+
+</div>
+  `
+  body.insertAdjacentHTML( 'afterbegin', htmlPage );
+} );
+
